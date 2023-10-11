01eae3b87a22ca5f28486c20d80e66a256e30064
Arion1822 <102961275+Arion1822@users.noreply.github.com>
2023-10-11 18:22:17 +0200

    no lo se

--- Practica1_FPS/Assets/Scripts/Character.cs
+++ Practica1_FPS/Assets/Scripts/Character.cs
@@ -11,6 +11,7 @@ public class Character : MonoBehaviour
     Rigidbody rb;
 
     private float rotation;
+    private bool isGrounded;
 
     void Start(){
         camera = GetComponentInChildren<Camera>();
@@ -28,6 +29,9 @@ public class Character : MonoBehaviour
         camera.transform.localRotation = Quaternion.Euler(rotation, 0, 0);
 
         Move();
+        isGrounded= RayCastGrounded();
+        Jumping();
+
     }
 
     void Move(){
@@ -49,5 +53,61 @@ public class Character : MonoBehaviour
         // Move the rigidbody using rb.MovePosition
         rb.MovePosition(rb.position + finalMovement);
     }
+
+
+    private Vector2 velocityGunFollow;
+    private float gunWeightX,gunWeightY;
+    [Tooltip("Current weapon that player carries.")]
+
+    private GameObject weapon;
+    private GunScript gun;
+/*
+ * Rotating current weapon from here.
+ * Checkig if we have a weapon, if we do, if its a gun it iwll fetch the gun and rotate it accordingly,
+ * same goes for the sword.
+ * Incase we dont have a weapon or gun or it didnt find it, it will write into the console that it cant find a weapon.
+ */
+void WeaponRotation(){
+	if(!weapon){
+		weapon = GameObject.FindGameObjectWithTag("Weapon");
+		if(weapon){
+			if(weapon.GetComponent<GunScript>()){
+				try{
+					gun = GameObject.FindGameObjectWithTag("Weapon").GetComponent<GunScript>();
+				}catch(System.Exception ex){
+					print("gun not found->"+ex.StackTrace.ToString());
+				}
+			}
+		}
+	}
+
+}
+
+public float jumpForce;
+void Jumping(){
+		if (Input.GetKeyDown (KeyCode.Space) && isGrounded) {
+			rb.AddRelativeForce (Vector3.up * jumpForce);
+
+		}
+	}
+
+
+
+    private bool RayCastGrounded(){
+		RaycastHit groundedInfo;
+		if(Physics.Raycast(transform.position, transform.up *-1f, out groundedInfo, 1)){
+			Debug.DrawRay (transform.position, transform.up * -1f, Color.red, 0.0f);
+			if(groundedInfo.transform != null){
+				
+				return true;
+			}
+			else{
+				
+				return false;
+			}
+		}
+
+		return false;
+	}
 
 }
